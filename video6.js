5662bcc350cea3c6b04b36ae6ad91a2b2029576e
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-11-10 20:11:00 +0600

    video 6 debuggin

--- video6.js
+++ video6.js
@@ -1,13 +1,15 @@
 // console.log(binarySearch([3, 5, 6, 7, 9, 11], 7)); // Output: 3
 
 const binarySearch = (arr, target) => {
-  let low = 0;
-  let high = arr.length - 1;
+  let low = 0; // 0 -> 3 ->
+  let high = arr.length - 1; // 5 -> 3 ->
 
   while (low <= high) {
-    const mid = Math.floor((high + low) / 2); // 2
-    const guess = arr[mid]; // 6
+    const mid = Math.floor((high + low) / 2); // 2 -> 4 -> 3
+    const guess = arr[mid]; // 6 -> 9 -> 7
+    console.log("mid", low, mid, guess);
 
+    // ? target -> 7
     if (guess === target) {
       console.log(mid);
       return mid;
